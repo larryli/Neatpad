5afff26e88169f48451d50fd68b84d189496b12b
Larry Li <larryli@qq.com>
2022-08-29 17:31:15 +0800

    http://www.catch22.net/tuts/neatpad/mouse-selection-highlighting

---
+++ Neatpad05/TextView/TextViewFile.c
@@ -0,0 +1,52 @@
+//
+//	MODULE:		TextViewFile.cpp
+//
+//	PURPOSE:	TextView file input routines
+//
+//	NOTES:		www.catch22.net
+//
+#include <tchar.h>
+#include <windows.h>
+
+#include "TextView.h"
+#include "TextViewInternal.h"
+
+//
+//
+//
+LONG TextView__OpenFile(TEXTVIEW *ptv, TCHAR *szFileName)
+{
+    TextView__ClearFile(ptv);
+
+    if (TextDocument__init_filename(ptv->m_pTextDoc, szFileName)) {
+        ptv->m_nLineCount = TextDocument__linecount(ptv->m_pTextDoc);
+        ptv->m_nLongestLine = TextDocument__longestline(ptv->m_pTextDoc, 4);
+
+        ptv->m_nVScrollPos = 0;
+        ptv->m_nHScrollPos = 0;
+
+        TextView__UpdateMetrics(ptv);
+        return TRUE;
+    }
+
+    return FALSE;
+}
+
+//
+//
+//
+LONG TextView__ClearFile(TEXTVIEW *ptv)
+{
+    if (ptv->m_pTextDoc)
+        TextDocument__clear(ptv->m_pTextDoc);
+
+    ptv->m_nLineCount = TextDocument__linecount(ptv->m_pTextDoc);
+    ptv->m_nLongestLine = TextDocument__longestline(ptv->m_pTextDoc, 4);
+
+    ptv->m_nVScrollPos = 0;
+    ptv->m_nHScrollPos = 0;
+
+    TextView__UpdateMetrics(ptv);
+
+    return TRUE;
+}
